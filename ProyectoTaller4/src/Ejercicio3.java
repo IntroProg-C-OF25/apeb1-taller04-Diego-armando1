bda897604be7cda882103d7e412d1068e543edd9
Diego-armando1 <guamangualandiegoarmando@gmail.com>
2024-11-03 08:26:15 -0500

    segundo commit

--- ProyectoTaller4/src/Ejercicio3.java
+++ ProyectoTaller4/src/Ejercicio3.java
@@ -5,13 +5,13 @@ public class Ejercicio3 {
         Scanner teclado = new Scanner(System.in);
         System.out.println("ingrese el numero: ");
         numero = teclado.nextDouble();
-          if (numero >= 2 && numero <= 6) {
-            System.out.println("Tabla de multiplicar del " + numero + ":");
-            for (int i = 1; i <= 10; i++) {
-                System.out.println(numero + " x " + i + " = " + (numero * i));
+        if (numero >= 2 && numero <= 6) {
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+        System.out.println(numero + " x " + i + " = " + (numero * i));
             }
         } else {
-            System.out.println("El número ingresado no está en el rango permitido (2-6).");
+        System.out.println("El número ingresado no está en el rango permitido (2-6).");
         }
     }
     
